9ca92e21c6ebdae2e16bf6483e3d97ef8dc54773
Stanislav Petr <glux@Stanislavs-MacBook-Pro.local>
2024-10-02 19:51:08 +0200

    Write output to CSV file

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ var (
 	flagStartPath = flag.String("path", "./", "start path")
 	flagTime      = flag.Int("time", 24, "time in hours")
 	flagMinSize   = flag.Int("min", 100, "min size in MB")
+	flagCSV       = flag.String("csv", "", "output in csv format")
+	csvFile       *os.File
 )
 
 func init() {
@@ -22,15 +24,33 @@ func init() {
 func main() {
 	fmt.Fprintf(os.Stderr, "TDU version: %s\n", version)
 	fmt.Fprintf(os.Stderr, "Modified before: %d\nMin increment size: %d\nStart path: %s\n", *flagTime, *flagMinSize, *flagStartPath)
+
+	if *flagCSV != "" {
+		fmt.Fprintf(os.Stderr, "Output in CSV format: %s\n", *flagCSV)
+		var err error
+		csvFile, err = os.Create(*flagCSV)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating file %s: %s\n", *flagCSV, err)
+			os.Exit(1)
+		}
+	}
+
 	processDirectory(*flagStartPath)
+
+	if csvFile != nil {
+		csvFile.Close()
+	}
 }
 
 func processDirectory(currentPath string) {
+	// Read directory
 	items, err := os.ReadDir(currentPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading directory %s: %s\n", currentPath, err)
 		return
 	}
+
+	// Process items in directory
 	directories := []string{}
 	sizeSum := int64(0)
 	for i := range items {
@@ -48,11 +68,17 @@ func processDirectory(currentPath string) {
 		}
 	}
 
+	// Report if sizeSum is greater than min size
 	if sizeSum > int64(*flagMinSize)*1024*1024 {
 		fmt.Printf("%s: %d MB\n", currentPath, sizeSum/1024/1024)
+		// Write to CSV file
+		if csvFile != nil {
+			fmt.Fprintf(csvFile, "%s,%d\n", currentPath, sizeSum/1024/1024)
+		}
 	}
 
-	for _, directory := range directories {
-		processDirectory(directory)
+	// Process subdirectories
+	for i := range directories {
+		processDirectory(directories[i])
 	}
 }
